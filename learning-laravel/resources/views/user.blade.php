7fe048102131d5b5def5931d6c63d98691dcdde5
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-31 00:31:18 +0300

    done  day 1

--- learning-laravel/resources/views/user.blade.php
+++ learning-laravel/resources/views/user.blade.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Greeting page</title>
+    <title>Greeting Page</title>
 </head>
 <body>
-    <h1>Greeting page</h1>
-    <p>Hello {{$name}}! wellcome to our sit</p>
-    <p>Hello our user {{$id}}</p>
+    <h1>Greeting Page</h1>
+    <p>Hello {{ $name }}! Welcome to our site.</p>
+    <p>Your ID is {{ $id }}</p>
 </body>
 </html>
- 
--- learning-laravel/routes/web.php
+++ learning-laravel/routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('home');
 });
-Route::get('/greet/{name}', function ($name) {
-    return view('greet',['name' => $name]);
+Route::get('/user/{name}', function ($id) {
+    return view('user',['name' => $name]);
 });
-Route::get('/user/{id}', function ($id) {
-    return view('user',['id' => $id]);
+Route::get('/user/{name}/{id}', function ($name, $id) {
+    return view('user', ['name' => $name, 'id' => $id]);
 });
+
