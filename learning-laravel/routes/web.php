56310c1ad492f515ef39ab06a6b1382d95cc9d52
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-31 00:00:57 +0300

    finish the first try

--- learning-laravel/routes/web.php
+++ learning-laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
