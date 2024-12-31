c76142a84a5ccd5e53665b7464942930aaca1e70
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-31 14:56:50 +0300

    check how to use templete

--- learning-laravel/routes/web.php
+++ learning-laravel/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::get('/', function(){
-    return view('home');
+Route::get('/welcome', function(){
+    return view('welcome');
 });
-Route::get('/user/{name}', function ($id) {
-    return view('user',['name' => $name]);
-});
-Route::get('/user/{name}/{id}', function ($name, $id) {
-    return view('user', ['name' => $name, 'id' => $id]);
-});
+
+// Route::get('/user/{name}', function ($id) {
+//     return view('user',['name' => $name]);
+// });
+// Route::get('/user/{name}/{id}', function ($name, $id) {
+//     return view('user', ['name' => $name, 'id' => $id]);
+// });
 
